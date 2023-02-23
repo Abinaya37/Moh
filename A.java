754596dc4bb2ba474e792e923acad88815ad2363
Abinaya37 <abinaya30091998@gmail.com>
2023-02-23 10:43:01 +0530

    Committed

---
+++ A.java
@@ -0,0 +1,21 @@
+package org.pojo;
+
+public class A {
+
+	private int salary = 12;
+
+	public int getSalary() {
+
+		return salary;
+
+	}
+
+	// int ab = 30000
+	// int salary = ab;
+	public void setSalary(int ab) {
+
+		this.salary = ab;
+
+	}
+
+}
